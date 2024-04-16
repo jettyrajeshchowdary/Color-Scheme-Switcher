2cda8ba3a82e261d0bc7a53a57f172c998c2ed9d
( •_•) Ｒａｊｅｓｈ Ｃｈｏｗｄａｒｙ( •_•) <154892930+jettyrajeshchowdary@users.noreply.github.com>
2024-04-16 20:05:38 +0530

    Update p1_script.js

--- p1_script.js
+++ p1_script.js
@@ -1,5 +1,6 @@
       const buttons = document.querySelectorAll(".button");
 const body = document.querySelector("body");
+
 buttons.forEach(function(button){
     button.addEventListener('click',function(e){ 
         if(e.target.id == 'grey'){
