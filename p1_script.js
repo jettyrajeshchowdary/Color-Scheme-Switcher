e282d697a4558e0d2f5e51cade1e1afce4d6c843
( •_•) Ｒａｊｅｓｈ Ｃｈｏｗｄａｒｙ( •_•) <154892930+jettyrajeshchowdary@users.noreply.github.com>
2024-04-16 20:04:34 +0530

    Update p1_script.js

--- p1_script.js
+++ p1_script.js
@@ -1,7 +1,7 @@
 const buttons = document.querySelectorAll(".button");
 const body = document.querySelector("body");
 buttons.forEach(function(button){
-    button.addEventListener('click',function(e){
+    button.addEventListener('click',function(e){ 
         if(e.target.id == 'grey'){
             body.style.backgroundColor = e.target.id;
         }
